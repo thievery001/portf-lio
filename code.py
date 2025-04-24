4364dd7472606b34145ed35f9aba349249347a70
thievery001 <bielfelipe9842@gmail.com>
2025-04-23 22:05:48 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,25 @@
+#Inicio do Portifólio
+print("Olá pessoal, nós do grupo de desenvolvimento (Vinícius Taiguara, Paulo Vitor, Gabriel Felipe, Maycon Henrique e Giovane Henrique), estamos criando este portifólio para explicar para vocês um pouco do que estamos aprendendo até o momento no curso de Análise e Desenvolvimento de Sistemas")
+
+#Partindo para o assunto
+print("Dando início ao assunto, começaremos a falar do (print), que em português, significa imprimir, onde no Python, sua função é imprimir algo na tela. Uma observação importante é que para o (print) funcionar e imprima algo na dela, será necessário o uso das duas aspas ou aspas simples")
+print("Outra observação é que se usado as o (print) sem aspas, poderá acabando chamar a variavel que está sendo usada, caso contrario poderá trazer um erro no código, se não existir essa variavel em específico")
+
+#Exemplo
+print("como por exemplo: ")
+print("Olá mundo!")
+
+#Continuidade do Assunto
+print("Agora falaremos da função (input), que tem como finalidade fazer que o usuário possa digitar algo na dela, isso é mais utilizado para fazer perguntas para os usuários.")
+print("Uma observação importante é que em alguns caso do input vem acompanhado de algumas funções anteriores, como o float, int, str. Que em alguns casos são utilizados em casos mais específicos.")
+print("Em geral a função final do (input) é a digitação que poderá ser feita pelo usuário na tela.")
+print("Como por exemplo: ")
+print("nome = input(Digite seu nome: ")
+
+#Exemplo
+print("nome = input(Digite seu nome: )")
+print("Após o comando dado, o usuário poderá digitar na tela.")
+
 
 # Loops de repetição For e While
 
@@ -50,23 +72,6 @@ print('''senha_correta = "1234"
 
 print('Explicação: O loop continua até o usuário digitar a senha correta')
 
-#Inicio do Portifólio
-print("Olá pessoal, nós do grupo de desenvolvimento (Vinícius Taiguara, Paulo Vitor, Gabriel Felipe, Maycon Henrique e Giovane Henrique), estamos criando este portifólio para explicar para vocês um pouco do que estamos aprendendo até o momento no curso de Análise e Desenvolvimento de Sistemas")
-
-#Partindo para o assunto
-print("Dando início ao assunto, começaremos a falar do (print), que em português, significa imprimir, onde no Python, sua função é imprimir algo na tela. Uma observação importante é que para o (print) funcionar e imprima algo na dela, será necessário o uso das duas aspas ou aspas simples")
-print("Outra observação é que se usado as o (print) sem aspas, poderá acabando chamar a variavel que está sendo usada, caso contrario poderá trazer um erro no código, se não existir essa variavel em específico")
-
-#Exemplo
-print("como por exemplo: ")
-print("Olá mundo!")
-
-#Continuidade do Assunto
-print("Agora falaremos da função (input), que tem como finalidade fazer que o usuário possa digitar algo na dela, isso é mais utilizado para fazer perguntas para os usuários.")
-print("Uma observação importante é que em alguns caso do input vem acompanhado de algumas funções anteriores, como o float, int, str. Que em alguns casos são utilizados em casos mais específicos.")
-print("Em geral a função final do (input) é a digitação que poderá ser feita pelo usuário na tela.")
-print("Como por exemplo: ")
-print("nome = input(Digite seu nome: ")
 
 #Exemplo
 print("nome = input(Digite seu nome: )")
