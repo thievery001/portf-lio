b3e77fce28c824c813f04a4952056073b16d9437
thievery001 <bielfelipe9842@gmail.com>
2025-04-24 16:50:27 -0300

    Update code.py

--- code.py
+++ code.py
@@ -21,58 +21,108 @@ print("nome = input(Digite seu nome: )")
 print("Após o comando dado, o usuário poderá digitar na tela.")
 
 
-# Loops de repetição For e While
+#tipos de dados simples
 
-# Loop For
+#int - numero inteiro
+x = 10
+y = 6   
+z = 8
+#float - numero decimal
+a = 10.5
+b = 6.7
+c = 8.9
+#str - string (texto)
+nome = "Paulo"
+idade = "18"
+altura = "1.87"
 
-print(''' Loop For: O loop for em Python é uma estrutura de controle que permite iterar sobre uma sequência (como uma lista, uma string ou uma faixa de números), executando um bloco de código repetidamente para cada item da sequência.''')
+#conversoes entre etapas
 
-# Loop While
+#int para float 
 
-print(''' Loop While: O loop while é um dos métodos fundamentais em Python para realizar a repetição de um bloco de código enquanto uma condição específica é verdadeira. Essa estrutura permite que programas executem tarefas repetitivas com eficiência, sem a necessidade de escrever o mesmo código várias vezes.''')
+x = float(x) #10.0
 
-# Diferença de For e While
+#int para str
 
-print(''' A principal diferença entre um loop for e um loop while reside na sua forma de controlar o número de iterações. Um loop for é geralmente usado quando se conhece o número exato de iterações antecipadamente, enquanto um loop while é usado quando a iteração é condicionada a uma condição, que pode não ser o número de iterações.''')
+y = str(y) #6
 
-# Exemplo prático For
+#float para int
 
-print('''for i in range(1, 11):
-          print(i)''')
+a = int(a) #10
 
-print('''numeros = [5, 10, 3, 8]
-      soma = 0
+#float para str
 
-      for n in numeros:
-          soma += n
+a = str(a) #10.5
 
-          print("Soma:", soma)''')
+#str para int
 
-print('Explição exemplo 1: range(1, 11) vai de 1 até 10 (o 11 é exclusivo).')
+idade = int(idade) #18
 
-print('Explicação exemplo 2: O loop percorre a lista numeros e soma cada item')
+#str para float
 
-# Exemplo prático While
+altura = float(altura) #1.87
 
-print('''contador = 1
+print(x, y, a, idade, altura)
 
-      while contador <= 5:
-          print(contador)
-              contador += 1''')
 
-print('Explicação: Enquanto o contador for menor ou igual a 5, ele imprime e soma + 1')
+#Operações Aritméticas em Python
 
-print('''senha_correta = "1234"
-      senha = ""
+#Para que serve as Operações Aritméticas
+print("As operações aritméticas elas são a base de qualquer linguagem de programção, inluindo python, elas permite que nós possamos fazer cálculos simples e complexos que é utilizado em toda criação de programa em python")
+print("No python existem 7 operações artméticas, A Soma, A Subtração, A Multiplicação, A Divisão, A Divisão inteira, O Módulo e A Exponenciação")
 
-      while senha != senha_correta:
-          senha = input("Digite a senha: ")
+#As operãções, seus símbolos e sua utilização
 
-          print("Acesso liberado!")''')
+#A Soma, ela adiciona valores. É representada pelo símebolo (+)
+print('''soma = a + b
+         print("Soma (a+b):", soma)''')
 
-print('Explicação: O loop continua até o usuário digitar a senha correta')
+#A Sutração, ela subtrai o segundo valor do primeiro. É representada pelo símbolo (-)
+print('''subtracao = a - b
+         print("Subtração (a-b):", subtracao)''')
 
-# Estruturas Condicionais (if, elif, else)
+#A Multiplicação, ela multiplica todos os valores. É representada pelo símbolo (*)
+print('''multiplicacao = a * b
+         print("Multiplicação (a*b):", multiplicacao)''')
+
+#A Divisão, ela retorna um número float. É representada pelo símbolo (/)
+print('''divisao = a / b
+         print("Divisão (a/b):", divisao)''')
+
+#A Divisão inteira, ela descarta o valor decimal(float) e apenas imprime o valor inteiro. É representada pelos símbolos (//)
+print('''divisao_inteira = a // b
+         print("divisão inteira (a//b):", divisao_inteira)''')
+
+#O Módulo, ele retorna o resto da divisão. É representado pelo símbolo (%)
+print('''modulo = a % b
+      print("Módulo (a%b):", modulo)''')
+
+#A Exponenciação ela eleva o valor de 'a' à potência de 'b'. È representada pelos símbolos (**)
+print('''exponenciacao = a ** b
+         print("Explnenciação (a**b):", exponenciacao)''')
+
+#Ordem de procedência
+print("O python ele respeita uma ordem de prioridade entre os operadores. Parênteses > Exponenciação > Multiplicação|Divisão > Soma|Subtração")
+
+#Exemplos
+print("n1 = 3 + 3 * 7 ** 2
+       print(n1)")
+#cálculo:
+print("7 ** 2  =  49
+       3 * 49  =  147
+       3 + 147 =  150
+       Resultado: 150")
+
+print("n2 = 3 * 6 + 4 + (7 % 2) 3 ** 3
+       print(n2)")
+#cálculo:
+print("7 % 2  =  1
+       3 ** 3 =  9
+       3 * 6  =  16
+       16 + 4 + 1 + 9 = 30
+       Resultado : 30")
+
+#Estruturas Condicionais (if, elif, else)
 
 print("Agora vamos falar sobre as estruturas condicionais, que são usadas quando precisamos que nosso programa tome decisões.")
 
@@ -124,104 +174,53 @@ elif opcao == "3":
 else:
     print("Opção inválida!")
 
-#tipos de dados simples
+# Loops de repetição For e While
 
-#int - numero inteiro
-x = 10
-y = 6   
-z = 8
-#float - numero decimal
-a = 10.5
-b = 6.7
-c = 8.9
-#str - string (texto)
-nome = "Paulo"
-idade = "18"
-altura = "1.87"
+# Loop For
 
-#conversoes entre etapas
+print(''' Loop For: O loop for em Python é uma estrutura de controle que permite iterar sobre uma sequência (como uma lista, uma string ou uma faixa de números), executando um bloco de código repetidamente para cada item da sequência.''')
 
-#int para float 
+# Loop While
 
-x = float(x) #10.0
+print(''' Loop While: O loop while é um dos métodos fundamentais em Python para realizar a repetição de um bloco de código enquanto uma condição específica é verdadeira. Essa estrutura permite que programas executem tarefas repetitivas com eficiência, sem a necessidade de escrever o mesmo código várias vezes.''')
 
-#int para str
+# Diferença de For e While
 
-y = str(y) #6
+print(''' A principal diferença entre um loop for e um loop while reside na sua forma de controlar o número de iterações. Um loop for é geralmente usado quando se conhece o número exato de iterações antecipadamente, enquanto um loop while é usado quando a iteração é condicionada a uma condição, que pode não ser o número de iterações.''')
 
-#float para int
+# Exemplo prático For
 
-a = int(a) #10
+print('''for i in range(1, 11):
+          print(i)''')
 
-#float para str
+print('''numeros = [5, 10, 3, 8]
+      soma = 0
 
-a = str(a) #10.5
+      for n in numeros:
+          soma += n
 
-#str para int
+          print("Soma:", soma)''')
 
-idade = int(idade) #18
+print('Explição exemplo 1: range(1, 11) vai de 1 até 10 (o 11 é exclusivo).')
 
-#str para float
+print('Explicação exemplo 2: O loop percorre a lista numeros e soma cada item')
 
-altura = float(altura) #1.87
+# Exemplo prático While
 
-print(x, y, a, idade, altura)
+print('''contador = 1
 
+      while contador <= 5:
+          print(contador)
+              contador += 1''')
 
-#Operações Aritméticas em Python
+print('Explicação: Enquanto o contador for menor ou igual a 5, ele imprime e soma + 1')
 
-#Para que serve as Operações Aritméticas
-print("As operações artméticas elas são a base de qualquer linguagem de programção, inluindo python, elas permite que nós possamos fazer cálculos simples e complexos que é utilizado é basicamente em toda criação de programa em python")
-print("No python existem 7 operações artméticas, A Soma, A Subtração, A Multiplicação, A Divisão, A Divisão inteira, O Módulo e A Exponenciação")
+print('''senha_correta = "1234"
+      senha = ""
 
-#As operãções, seus símbolos e sua utilização
+      while senha != senha_correta:
+          senha = input("Digite a senha: ")
 
-#A Soma, ela adiciona valores. È representada pelo símebolo (+)
-print('''soma = a + b
-         print("Soma (a+b):", soma)''')
+          print("Acesso liberado!")''')
 
-#A Sutração, ela subtrai o segundo valor do primeiro. È representada pelo símbolo (-)
-print('''subtracao = a - b
-         print("Subtração (a-b):", subtracao)''')
-
-#A Multiplicação, ela multiplica todos os valores. È representada pelo símbolo (*)
-print('''multiplicacao = a * b
-         print("Multiplicação (a*b):", subtracao)''')
-
-#A Divisão, ela retorna um número float. È representada pelo símbolo (/)
-print('''divisao = a / b
-         print("Divisão (a/b):", divisao)''')
-
-#A Divisão inteira, ela descarta o valor decimal(float) e apenas imprime o valor inteiro. È representada pelos símbolos (//)
-print('''divisao_inteira = a // b
-         print("divisão inteira (a//b):", divisao_inteira)''')
-
-#O Módulo, ele retorna o resto da divisão. É representado pelo símbolo (%)
-print('''modulo = a % b
-      print("Módulo (a%b):", modulo)''')
-
-#A Exponenciação ela eleva o valor de 'a' à potência de 'b'. È representada pelos símbolos (**)
-print('''exponenciacao = a ** b
-         print("Explnenciação (a**b):", exponenciacao)''')
-
-
-#Ordem de procedência
-print("O python ele respeita uma ordem de prioridade entr os operadores. Parênteses > Exponenciação > Multiplicação|Divisão > Soma|Subtração")
-
-#Exemplos
-print("n1 = 3 + 3 * 7 ** 2
-       print(n1)")
-#cálculo:
-print("7 ** 2  =  49
-       3 * 49  =  147
-       3 + 147 =  150
-       Resultado: 150")
-
-print("n2 = 3 * 6 + 4 + (7 % 2) 3 ** 3
-       print(n2)")
-#cálculo:
-print("7 % 2  =  1
-       3 ** 3 =  9
-       3 * 6  =  16
-       16 + 4 + 1 + 9 = 30
-       Resultado : 30")
+print('Explicação: O loop continua até o usuário digitar a senha correta')
